82a1ef322b37ad5f9961c29ab9bb4f52cdec9fb1
Aayush Kc <aayushkc@Aayushs-MacBook-Pro.local>
2020-09-02 20:41:54 +0545

    Spring data Redis

---
+++ src/model/User.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.io.Serializable;
+
+public class User  implements Serializable {
+
+    private int id;
+    private String name;
+    private Long salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+
+    public User(int id, String name, Long salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
